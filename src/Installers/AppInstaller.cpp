b48590a7cee5a3f8a50440e4bf8d7e3ff78bb230
mineblock11 <93472213+mineblock11@users.noreply.github.com>
2022-12-26 00:04:40 +0000

    includes

--- src/Installers/AppInstaller.cpp
+++ src/Installers/AppInstaller.cpp
@@ -1,8 +1,12 @@
 #include "Installers/AppInstaller.hpp"
 #include "Services/RamCleaner.hpp"
 
+#include "Zenject/ConcreteIdBinderGeneric_1.hpp"
+#include "Zenject/DiContainer.hpp"
+#include "Zenject/FromBinderNonGeneric.hpp"
+
 DEFINE_ZENJECT_INSTALLER(AppInstaller) {
-    INFO("AppInstaller::InstallBindings")
+    INFO("AppInstaller::InstallBindings");
 
     auto container = get_Container();
 
